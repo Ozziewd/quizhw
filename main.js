4e5936ea1bc5974b25c5e8c5119fb3a6ee200225
Osvaldo Ruiz <owebstudent@gmail.com>
2019-10-25 02:32:10 -0400

    .

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 var startBtn = document.getElementById("startB");
+var questionHeading = document.getElementById("questions");
+var answerslist = document.getElementById("answers");
 startBtn.addEventListener("click", function (event) {
     document.getElementById("startscreen").classList.add("d-none")
     document.getElementById("questionscreen").classList.remove("d-none")
-
-        
 });
+questionHeading.textContent = questions[0].title;
+answerslist.innerHTML = "";    
+var choices = questions[0].choices;
+for (var i = 0; i < choices.length; i++) {
+    var newChoice = document.createElement("div");
+    newChoice.textContent = choices[i];
+    answerslist.appendChild(newChoice);
+} 
