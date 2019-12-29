55e7e7bb61bc1c3f8675829d6874a09b781b325b
Osvaldo Ruiz <owebstudent@gmail.com>
2019-12-29 12:25:39 -0500

    changed title from file index.js to questions.js and changed it also in index.html

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ var answerslist = document.getElementById("answers");
 var timeDom = document.getElementById('timer');
 var count = 0
 var timeInterval
-var timeLeft = questions.length*15;
-const timed = ()=>{
+var timeLeft = questions.length * 15;
+const timed = () => {
 
-    timeInterval = setInterval(function() {
-    timeLeft--;
-    timeDom.textContent = 'Time left: '+ timeLeft
-    if (timeLeft === 0) {
-      clearInterval(timeInterval);
-    }
+    timeInterval = setInterval(function () {
+        timeLeft--;
+        console.log(timeLeft)
+        timeDom.textContent = 'Time left: ' + timeLeft
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+        }
 
-  }, 1000);
+    }, 1000);
 }
 startBtn.addEventListener("click", function (event) {
     document.getElementById("startscreen").classList.add("d-none")
@@ -23,36 +24,36 @@ startBtn.addEventListener("click", function (event) {
     printButtons()
 });
 
-const printButtons =()=>{
+const printButtons = () => {
     questionHeading.textContent = questions[count].title;
-    answersList.innerHTML = "";    
+    answersList.innerHTML = "";
     var choices = questions[count].choices;
     for (var i = 0; i < choices.length; i++) {
         var newChoice = document.createElement("div");
-        newChoice.innerHTML = "<button onclick = check('"+choices[i]+"')>"+choices[i]+"</button>";
+        newChoice.innerHTML = "<button onclick = check('" + choices[i] + "')>" + choices[i] + "</button>";
         answersList.appendChild(newChoice);
     }
 
 }
 var check = guess => {
-    if(questions[count].answer === guess){
+    if (questions[count].answer === guess) {
         console.log('you are correct')
     }
-    else{
+    else {
         timeLeft -= 10
         console.log('you are wrong')
     }
     count++
-    if(questions.length === count){
+    if (questions.length === count) {
         clearInterval(timeInterval)
         console.log('game over')
         endGame()
     }
-    else{
+    else {
         printButtons()
-     }
+    }
 }
-const endGame =()=>{
-    questionHeading.innerHTML= '<h1> Game Over!</h1>'
+const endGame = () => {
+    questionHeading.innerHTML = '<h1> Game Over!</h1>'
     answersList.innerHTML = '<input> Initials here'
 }
