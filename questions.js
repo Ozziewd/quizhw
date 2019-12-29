9763f9bd991f4b44e69c04d28fcae9103c2b568a
Osvaldo Ruiz <owebstudent@gmail.com>
2019-12-29 03:10:16 -0500

    added a few more questions and updated the read me

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly&nbspbrackets", "parentheses", "square&nbspbrackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "what command do you use to show a prompt?",
+      choices: ["alerts","prompts","bolean","forum"],
+      answer:"prompts"
+    },
+    {
+      title: "what code shows an alert?",
+      choices: ["alert","prompts","bolean","forum"],
+      answer:"alert"
+    },
+    {
+      title: "what values confirm return?",
+      choices: ["true and false","right and wrong","positive and negative","none"],
+      answer:"true and false"
+    }
+  ];
+  
